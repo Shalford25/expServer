e1badc37827d94bffea75e80dfaaecf871bb8af4
Shalford25 <spencer.halford@lander.edu>
2025-03-18 10:09:50 -0400

    added the connections

--- api/index.js
+++ api/index.js
@@ -1,85 +1,13 @@
 import express from "express";
 import cors from "cors";
-import pg from "pg";
-import dotenv from "dotenv";
-
+import bookRouter from "./routes/BookRoutes.js";
+import pool from "./routes/PoolConnection.js";
+import userRouter from "./routes/UserRoutes.js";
 const app=express();
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/shop', bookRouter);
+app.arguments("/users",userRouter);
 
-dotenv.config(); // Load environment variables
-const { Pool } = pg;
-
-const PORT = process.env.PORT || 3000;
-
-// PostgreSQL connection pool
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Required for Neon DB
-  },
-});
-
-pool.connect()
-  .then(client => {
-    console.log("Connected to Neon PostgreSQL database!");
-    client.release();
-  })
-  .catch(err => console.error("Database connection error:", err.stack));
-
-app.get("/", async (req, res) => {
-  // What you get (req) and what you send (res)
-    try {
-     
-          res.send("Hello from Express Server");
-     
-    } catch (error) {
-      console.error("Query error:", error);
-      res.send(" Sorry Error")
-     
-    }
-  });
-  app.get("/books", async (req, res) => {
-    try {
-      const result = await pool.query("SELECT * from books");
-      res.json({ rows:result.rows });
-   //   console.log(result.rows.length);
-    } catch (error) {
-      console.error("Query error:", error);
-      res.status(500).json({ error: "Database query failed" });
-
-    }
-  });
-
-  app.get("/getbook", async (req, res) => {
-    try {
-      var id1=req.query.id;
-      console.log(id1);
-      const result = await pool.query("select * from books where id="+id1);
-      console.log(result);
-      res.json({rows:result.rows});
-     
-    } catch (error) {
-      console.error("Query error:", error);
-      res.json({rows:[]});
-     
-    }
-  });
-
-  app.get("/deletebook", async (req, res) => {
-    try {
-      var id1=req.query.id;
-      console.log(id1);
-      const result = await pool.query("DELETE FROM books WHERE id="+id1);
-      console.log(result);
-      res.json({ans:1});
-     
-    } catch (error) {
-      console.error("Query error:", error);
-      res.json({ans:0});
-     
-    }
-  });
- //
- app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("Server ready on port 3000."));
